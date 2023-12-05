1f362ed5c854ea2d12e522a92078ae0c6fe37e9f
nick@nickcondon.com <124863043+nick-condon@users.noreply.github.com>
2023-12-05 13:05:12 +1100

    Inital Commit

---
+++ weather.py
@@ -0,0 +1,75 @@
+import requests
+import constants
+from datetime import datetime
+import sqlite3
+
+
+# Function to delete all records in a table
+def clear_database_table(db_name, table_name):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    del_query = 'DELETE FROM ' + table_name
+    cursor.execute(del_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+# Function to get the weather data and return it into an instance of a WeatherData class.
+def get_weather_forecast(location_array):
+    location = location_array[1]
+    lat = location_array[2]
+    lng = location_array[3]
+    API_key = constants.api_key_openweather
+    # requests API data using inputs
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?'
+                        f'lat={lat}'
+                        f'&lon={lng}'
+                        f'&appid={API_key}'
+                        f'&units=metric').json()
+
+    # create empty array to store forecast
+    weatherforecast = []
+
+    # create iterable object to go through the 'daily' array in the returned API
+    iter_object = iter(resp.get('daily'))
+    while True:
+        try:
+            day = next(iter_object)
+            data = (
+                None,
+                location,
+                datetime.fromtimestamp((day.get('dt'))).strftime('%d-%m-%Y'),
+                datetime.fromtimestamp((day.get('dt'))).strftime('%A'),
+                day.get('weather')[0].get('main'),
+                day.get('weather')[0].get('description'),
+                round(day.get('temp').get('min'), 1),
+                round(day.get('temp').get('max'), 1),
+                day.get('weather')[0].get('icon')
+            )
+            weatherforecast.append(data)
+        except StopIteration:
+            break
+    connection = sqlite3.connect('tourism.db')
+    cursor = connection.cursor()
+    cursor.executemany('INSERT INTO weather (id, location, date, day, weather_main, description, min_temp, max_temp, icon) VALUES (?,?,?,?,?,?,?,?,?)', weatherforecast)
+    connection.commit()
+    connection.close()
+
+
+def update_all_weather_forecasts():
+    clear_database_table('tourism.db', 'weather')
+    try:
+        connection = sqlite3.connect('tourism.db')
+        cur = connection.cursor()
+        sql_locations_query = """SELECT * FROM places"""
+        cur.execute(sql_locations_query)
+        locations = cur.fetchall()
+        cur.close()
+        for location in locations:
+            get_weather_forecast(location)
+    except sqlite3.Error as error:
+        print("Failed to read data from sqlite table", error)
+
+
+update_all_weather_forecasts()
